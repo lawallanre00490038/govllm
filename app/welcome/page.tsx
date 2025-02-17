7372c8f56765d23794bbaf96a57bcc1953fb5cfd
Olanrewaju <lawallanre.abbahouse@gmail.com>
2025-02-17 18:08:09 +0100

    feat: checked

--- app/welcome/page.tsx
+++ app/welcome/page.tsx
@@ -3,7 +3,7 @@
 import React from 'react';
 import Link from 'next/link';
 import Image from 'next/image';
-import './WelcomePage.css';
+import './welcomePage.css';
 
 const WelcomePage: React.FC = () => {
   return (
